3137972011100fd18a5ecbf9588a659113dbd6c0
NikNef <nicolas.firpo@davinci.edu.ar>
2024-11-04 11:58:23 -0300

    add: cors

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from "express"
 import apiCursoRoute from "./api/routes/cursos.routes.js"
 import apiClienteRoute from "./api/routes/clientes.routes.js"
+import cors from "cors"
 
 const app = express()
 
 app.use( express.static("public") )
 app.use( express.urlencoded({ extended: true }) )
 app.use( express.json() )
+
+const corsOptions = {
+    origin: "http://localhost:5173", // LearnHub-Front 
+    methods: "GET,POST,PUT,DELETE" //Metodos Permitidos
+
+}
+
+app.use( cors(corsOptions) )
 
 app.use("/api",apiCursoRoute)
 app.use("/api",apiClienteRoute)
